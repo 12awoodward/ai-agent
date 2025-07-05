8575a9fdac97f5c0d93d2ca3dbe1c18c4d99f01e
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-07-05 15:09:55 +0100

    pass in prompt when running main

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import os
+from sys import argv
+
 from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
-
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    prompt = getPrompt()
+    if prompt:
+
+        messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+        
+        print(response.text)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
+    else:
+        print("Error: Missing Prompt")
+        exit(1)
+
+def getPrompt():
+    if len(argv) >= 2:
+        return argv[1]
+    return None
 
 if __name__ == "__main__":
     main()
